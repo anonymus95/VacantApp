080399ed21bd475da16072f958cb9aaacd57a656
anonymus95 <ingsotfwareuniquindio@gmail.com>
2019-06-21 07:41:29 -0500

    se agrego nueva tabla : Aspirants

---
+++ database/migrations/2019_06_21_070922_craate_aspirants_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CraateAspirantsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('aspirants', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->string('last_name');
+            $table->string('email');
+            $table->string('hv');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('aspirants');
+    }
+}
