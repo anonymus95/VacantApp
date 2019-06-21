143da74859a725e2eb0b48157ad2197294f5fc6b
anonymus95 <ingsotfwareuniquindio@gmail.com>
2019-06-21 12:48:39 -0500

    se agrego enlaces de crear vacante y postularme a una vacante

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -41,6 +41,9 @@
                         <!-- Authentication Links -->
                         @guest
                             <li class="nav-item">
+                                <a class="nav-link" href="/aspirants/create">Postularme a una vacante</a>
+                            </li>
+                            <li class="nav-item">
                                 <a class="nav-link" href="{{ route('login') }}">{{ __('Inicio de sesión') }}</a>
                             </li>
                             @if (Route::has('register'))
@@ -49,6 +52,9 @@
                                 </li>
                             @endif
                         @else
+                            <li class="nav-item">
+                                <a class="nav-link" href="/vacants/create">Crear una vacante</a>
+                            </li>
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }} <span class="caret"></span>
